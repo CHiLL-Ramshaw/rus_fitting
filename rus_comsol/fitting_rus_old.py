5ab364b54ed56e952a5b67c0004f137ddfec99d6
Gael <gael.phys@gmail.com>
2021-03-28 15:50:52 -0400

    Migration to Ray, still working for multiprocessing

--- rus_comsol/fitting_rus_old.py
+++ rus_comsol/fitting_rus_old.py
@@ -2,48 +2,52 @@ import numpy as np
 from scipy.spatial import distance_matrix
 from scipy.optimize import differential_evolution, linear_sum_assignment
 from multiprocessing import cpu_count, Pool
+# from pathos.multiprocessing import ProcessingPool as Pool
 import mph
 import time
 from copy import deepcopy
 import sys
 import json
+
 ##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-class RUSFitting:
-    def __init__(self, rus_object, ranges,
-                 freqs_file,
+class FittingRUS:
+    def __init__(self, init_member, ranges_dict,
+                 freqs_file, mph_file,
                  nb_freq_data, nb_freq_sim,
                  missing=True,
-                 nb_workers=4,
+                 study_name="resonances",
+                 method="differential_evolution", parallel=False, nb_workers=1,
                  population=15, N_generation=10000, mutation=0.1, crossing=0.9,
                  polish=False, updating='deferred', tolerance = 0.01,
                  **trash):
-        """
-        - if nb_workers = 1, the calculation is in series
-        """
-        self.rus_object = rus_object
-        self.init_pars  = deepcopy(self.rus_object.pars)
-        self.ranges     = ranges
+        ## Initialize
+        self.init_member  = deepcopy(init_member)
+        self.member       = deepcopy(init_member)
+        self.ranges_dict  = ranges_dict
         try:
             assert nb_freq_sim >= nb_freq_data
         except AssertionError:
             print("You need --- nb_freq_sim > nb_freq_data")
             sys.exit(1)
-        self.nb_freq_data     = nb_freq_data
-        self.nb_freq_sim      = nb_freq_sim
-        self.freqs_file       = freqs_file
-        self.missing          = missing
-        self.freepars_name    = sorted(self.ranges.keys())
-        self.fixedpars_name   = np.setdiff1d(sorted(self.init_pars.keys()), self.freepars_name)
+        self.nb_freq_data = nb_freq_data
+        self.nb_freq_sim  = nb_freq_sim
+        self.freqs_file   = freqs_file
+        self.mph_file     = mph_file
+        self.missing      = missing
+        self.study_name   = study_name
+        self.method       = method # "shgo", "differential_evolution", "leastsq"
+        self.pars_name    = sorted(self.ranges_dict.keys())
+        self.fixed_name   = np.setdiff1d(sorted(self.init_member.keys()), self.pars_name)
 
         ## Create list of bounds
-        self.freepars_bounds  = []
-        for free_name in self.freepars_name:
-            self.freepars_bounds.append((self.ranges[free_name][0],
-                                         self.ranges[free_name][-1]))
-        self.freepars_bounds = tuple(self.freepars_bounds)
+        self.pars_bounds  = []
+        for param_name in self.pars_name:
+            self.pars_bounds.append((ranges_dict[param_name][0], ranges_dict[param_name][-1]))
+        self.pars_bounds = tuple(self.pars_bounds)
 
         ## Differential evolution
+        self.parallel     = parallel
         self.nb_workers   = nb_workers
         self.population   = population # (popsize = population * len(x))
         self.N_generation = N_generation
@@ -57,6 +61,8 @@ class RUSFitting:
         self.nb_calls   = 0
         self.json_name  = None
         self.freqs_data = None # in MHz
+        # self.client     = None
+        # self.model      = None
 
         ## Load data
         self.load_data()
@@ -83,11 +89,26 @@ class RUSFitting:
         return index_sim, freqs_sim[index_sim]
 
 
-    def sort_freqs(self, freqs_sim_calc):
+    def objective_func(self):
+        start_total_time = time.time()
+
+        ## Update elastic constants --------------------------------------------------
+        for param_name in self.pars_name:
+            model.parameter(param_name, str(self.member[param_name][0]) +  "[" + self.member[param_name][1] + "]")
+
+        ## Compute resonances --------------------------------------------------------
+        model.solve(self.study_name)
+        freqs_sim_calc = model.evaluate('abs(freq)', 'MHz')
+        ## Remove the useless small frequencies
+        freqs_sim_calc = freqs_sim_calc[freqs_sim_calc > 1e-4]
+        model.clear()
+        model.reset()
+
         if self.missing == True:
             ## Linear assignement of the simulated frequencies to the data
             index_sim, freqs_sim = self.assignement(self.freqs_data, freqs_sim_calc)
-            ## Give the missing frequencies in the data -------------------------
+
+            ## Give the missing frequencies in the data -----------------------------
             # Let's remove the extra simulated frequencies that are beyond
             # the list of data frequencies
             bool_missing = np.ones(freqs_sim_calc.size, dtype=bool)
@@ -95,49 +116,61 @@ class RUSFitting:
             index_missing = np.arange(0, freqs_sim_calc.size, 1)[bool_missing]
             index_missing = index_missing[index_missing < self.freqs_data.size]
             freqs_missing = freqs_sim_calc[index_missing]
+
             print("Missing frequencies ---", freqs_missing, " MHz")
+
         else:
+            ## Remove the first 6 bad frequencies
+            # freqs_sim = freqs_sim[6:]
+            freqs_sim = freqs_sim_calc[freqs_sim_calc > 1e-4]
+
             ## Only select the first number of "freq to compare"
-            freqs_sim = freqs_sim_calc[:self.nb_freq_data]
+            freqs_sim = freqs_sim[:self.nb_freq_data]
+
+        self.nb_calls += 1
+        print("---- call #" + str(self.nb_calls) + " in %.6s seconds ----" % (time.time() - start_total_time))
+        sys.stdout.flush()
+
         return freqs_sim
 
 
-    def compute_chi2(self, freepars_array):
-        ## Update pars with fit parameters
-        for i, free_name in enumerate(self.freepars_name):
-            self.rus_object.pars[free_name][0] = freepars_array[i]
-            print(free_name
-                  + " : "
-                  + "{0:g}".format(self.rus_object.pars[free_name][0])
-                  + " "
-                  + self.rus_object.pars[free_name][1])
-
-        start_total_time = time.time()
-        ## Compute resonances ---------------------------------------------------
-        freqs_sim_calc = self.rus_object.compute_freqs()
-        ## Remove the first 6 bad frequencies
-        # freqs_sim = freqs_sim[6:]
-        ## Remove the useless small frequencies
-        freqs_sim_calc = freqs_sim_calc[freqs_sim_calc > 1e-4]
-        freqs_sim = self.sort_freqs(freqs_sim_calc)
-
-        # self.nb_calls += 1
-        # print("---- call #" + str(self.nb_calls) + " in %.6s seconds ----" % (time.time() - start_total_time))
-        print("---- call in %.6s seconds ----" % (time.time() - start_total_time))
-        sys.stdout.flush()
-
+    def compute_chi2(self, pars_array):
+        ## Update member with fit parameters
+        for i, param_name in enumerate(self.pars_name):
+            self.member[param_name][0] = pars_array[i]
+            print(param_name + " : " + "{0:g}".format(self.member[param_name][0]) + " " + self.member[param_name][1])
+        freqs_sim = self.objective_func()
+        # freqs_sim = pars_array
+        # self.freqs_data = np.array([321, 103])
         return np.sum((freqs_sim - self.freqs_data)**2)
 
 
-    def generate_workers(self):
-        if self.nb_workers > 1:
+    def initiate_fit(self):
+        ## Initialize the COMSOL file
+        global client
+        global model
+        client = mph.Client()
+        model = client.load(self.mph_file)
+
+        #######################################################################
+        # !!!!CHANGE to self.member.keys() instead of just pars_name from ranges_dict
+        ########################################################################
+
+        for param_name in self.pars_name:
+            model.parameter(param_name, str(self.member[param_name][0]) +  "[" + self.member[param_name][1] + "]")
+
+        ## Modifying COMSOL parameters >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        model.parameter('nb_freq', str(self.nb_freq_sim + 6))
+
+        if self.parallel == True:
             print(">>>>  Worker Init <<<<")
             sys.stdout.flush()
 
 
     def run_fit(self):
+
         ## Initialize client
-        if self.nb_workers > 1:
+        if self.parallel == True:
             print("# of available cores: ", cpu_count())
             pool = Pool(processes=self.nb_workers, initializer=self.initiate_fit)
             workers = pool.map
@@ -153,7 +186,7 @@ class RUSFitting:
         t0 = time.time()
 
         ## Run fit algorithm
-        out = differential_evolution(self.compute_chi2, bounds=self.freepars_bounds,
+        out = differential_evolution(self.compute_chi2, bounds=self.pars_bounds,
                                      workers=workers, updating=self.updating,
                                      polish=self.polish,
                                      maxiter=self.N_generation,
@@ -164,8 +197,8 @@ class RUSFitting:
                                      )
 
         ## Export final parameters from the fit
-        for i, free_name in enumerate(self.freepars_name):
-            self.rus_object.pars[free_name][0] = out.x[i]
+        for i, param_name in enumerate(self.pars_name):
+            self.member[param_name][0] = out.x[i]
 
         ## Fit report
         duration    = np.round(time.time() - t0, 2)
@@ -177,7 +210,7 @@ class RUSFitting:
         print("\n")
         report = "#[[Fit Statistics]]" + "\n"
         report+= "\t# fit success        \t= " + str(out.success) + "\n"
-        report+= "\t# fitting method     \t= " + "differential evolutin" + "\n"
+        report+= "\t# fitting method     \t= " + self.method + "\n"
         report+= "\t# generations        \t= " + str(out.nit) + " + 1" + "\n"
         report+= "\t# function evals     \t= " + str(out.nfev) + "\n"
         report+= "\t# data points        \t= " + str(N_points) + "\n"
@@ -186,20 +219,20 @@ class RUSFitting:
         report+= "\t# chi-square         \t= " + r"{0:.8f}".format(chi2) + "\n"
         report+= "\t# reduced chi-square \t= " + r"{0:.8f}".format(reduced_chi2) + "\n"
         report+= "#[[Variables]]" + "\n"
-        for i, free_name in enumerate(self.freepars_name):
-            report+= "\t# " + free_name + " : " + r"{0:.8f}".format(out.x[i]) + " " + \
-                     self.rus_object.pars[free_name][1] + \
-                     " (init = [" + str(self.ranges[free_name][0]) + \
-                     ", " +         str(self.ranges[free_name][1]) + "])" + "\n"
+        for i, param_name in enumerate(self.pars_name):
+            report+= "\t# " + param_name + " : " + r"{0:.8f}".format(out.x[i]) + " " + \
+                     self.member[param_name][1] + \
+                     " (init = [" + str(self.ranges_dict[param_name][0]) + \
+                     ", " +         str(self.ranges_dict[param_name][1]) + "])" + "\n"
         report+= "#[[Fixed values]]" + "\n"
-        for fixedpars_name in self.fixedpars_name:
-            report+= "\t# " + fixedpars_name + " : " + \
-                     r"{0:.8f}".format(self.rus_object.pars[fixedpars_name][0]) + " " + \
-                     self.rus_object.pars[fixedpars_name][1] + "\n"
+        for fixed_name in self.fixed_name:
+            report+= "\t# " + fixed_name + " : " + \
+                     r"{0:.8f}".format(self.member[fixed_name][0]) + " " + \
+                     self.member[fixed_name][1] + "\n"
 
         print(report)
 
-        report_file = open(self.rus_object.mph_file[:-4] + "_fit_report.txt", "w")
+        report_file = open(self.mph_file[:-4] + "_fit_report.txt", "w")
         report_file.write(report)
         report_file.close()
 
@@ -208,7 +241,7 @@ class RUSFitting:
         #     client.clear()
 
         ## Close pool
-        if self.nb_workers > 1:
+        if self.parallel == True:
             pool.terminate()
 
 
---
+++ rus_comsol/rus_comsol.py
@@ -0,0 +1,47 @@
+import mph
+from copy import deepcopy
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+class RUSComsol:
+    def __init__(self, pars,
+                 mph_file,
+                 nb_freq,
+                 study_name="resonances",
+                 init=False,
+                 **trash):
+        self.pars         = deepcopy(pars)
+        self.pars_name    = sorted(self.pars.keys())
+        self._nb_freq     = nb_freq
+        self.mph_file     = mph_file
+        self.study_name   = study_name
+        self.client       = None
+        self.model        = None
+        self.freqs        = None
+        if init == True:
+            self.start_comsol()
+
+    ## Properties >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    def _get_nb_freq(self):
+        return self._nb_freq
+    def _set_nb_freq(self, nb_freq):
+        self._nb_freq = nb_freq
+        self.model.parameter('nb_freq', str(self._nb_freq + 6))
+    nb_freq = property(_get_nb_freq, _set_nb_freq)
+
+    def compute_freqs(self):
+        ## Set parameters  ------------------------------------------------------
+        for pars_name in self.pars_name:
+            self.model.parameter(pars_name, str(self.pars[pars_name][0]) +
+                                 "[" + self.pars[pars_name][1] + "]")
+        ## Compute resonances ---------------------------------------------------
+        self.model.solve(self.study_name)
+        self.freqs = self.model.evaluate('abs(freq)', 'MHz')
+        self.model.clear()
+        self.model.reset()
+        return self.freqs
+
+    def start_comsol(self):
+        """Initialize the COMSOL file"""
+        self.client = mph.Client()
+        self.model = self.client.load(self.mph_file)
+        self.model.parameter('nb_freq', str(self._nb_freq + 6))
